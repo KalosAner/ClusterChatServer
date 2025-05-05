c7761baa340924e5370d6260d2be47716ca6e095
KalosAner <mengyikenan6@gmail.com>
2025-05-05 17:12:19 +0800

    添加自动构建脚本

--- build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -4,15 +4,15 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/kalosaner/learn/chat/src/server/chatserver.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/chatservice.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/db/db.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/main.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/model/friendmodel.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/model/groupmodel.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/model/offlinemessagemodel.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/model/usermodel.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
-  "/home/kalosaner/learn/chat/src/server/redis/redis.cpp" "/home/kalosaner/learn/chat/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/chatserver.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/chatservice.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/db/db.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/main.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/model/friendmodel.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/model/groupmodel.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/model/offlinemessagemodel.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/model/usermodel.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/kalosaner/learn/ClusterChatServer/src/server/redis/redis.cpp" "/home/kalosaner/learn/ClusterChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
